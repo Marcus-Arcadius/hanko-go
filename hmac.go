d8f01d9f82b5be15b2f50539df62cea9dbe56434
Lennart Fleischmann <l.fleischmann@gmail.com>
2020-05-28 14:42:45 +0200

    Issue #1 Implement HMAC calculation

--- hmac.go
+++ hmac.go
@@ -1,3 +1,66 @@
 package hankoApiClient
 
-// TODO implement HMAC authentication
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"time"
+)
+
+type HmacMessageData struct {
+	apiSecret     string
+	apiKeyId      string
+	requestMethod string
+	requestPath   string
+	requestBody   string
+}
+
+type HmacJson struct {
+	ApiKeyId	string `json:"apiKeyId"`
+	Time		string `json:"time"`
+	Nonce		string `json:"nonce"`
+	Signature	string `json:"signature"`
+}
+
+func CalculateHmac(messageData *HmacMessageData) string {
+	date := time.Now().UTC().Format("2006-01-02T15:04:05.999Z")
+	nonce := uuid.New().String()
+
+	message := fmt.Sprintf("%s:%s:%s:%s:%s",
+		messageData.apiKeyId,
+		date,
+		messageData.requestMethod,
+		messageData.requestPath,
+		nonce,
+	)
+
+	if messageData.requestBody != "" {
+		hashedRequestBody := hashRequestBody(messageData.requestBody)
+		message = fmt.Sprintf("%s:%s", message, hashedRequestBody)
+	}
+
+	mac := hmac.New(sha256.New, []byte(messageData.apiSecret))
+	mac.Write([]byte(message))
+	signatureHex := hex.EncodeToString(mac.Sum(nil))
+
+	hmacJson := &HmacJson{
+		ApiKeyId:  messageData.apiKeyId,
+		Time:      date,
+		Nonce:     nonce,
+		Signature: signatureHex,
+	}
+
+	jsonRepresentation, _ := json.Marshal(hmacJson)
+
+	return base64.StdEncoding.EncodeToString(jsonRepresentation)
+}
+
+func hashRequestBody(requestBody string) string {
+	digest := sha256.New()
+	digest.Write([]byte(requestBody))
+	return hex.EncodeToString(digest.Sum(nil))
+}
