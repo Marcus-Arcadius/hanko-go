1eb7857789683c0501371ca15c1ae226d4fddfa0
Björn Müller <bjoern.mueller@hanko.io>
2021-04-27 14:47:02 +0200

    refactor: tests no longer have cross-package dependencies

--- client/client_test.go
+++ client/client_test.go
@@ -1,22 +1,63 @@
 package client
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 )
 
+const (
+	testPort         = ":9496"
+	testBaseUrl      = "http://" + testPort
+	testApiSecret    = "test"
+	testHmacApiKeyId = "test"
+)
+
+func runTestApi(requestType interface{}, response interface{}, responseStatus int) *httptest.Server {
+	ts := httptest.NewUnstartedServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+
+			if requestType != nil {
+				dec := json.NewDecoder(r.Body)
+				dec.DisallowUnknownFields()
+				err := dec.Decode(&requestType)
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+			}
+
+			w.WriteHeader(responseStatus)
+
+			if response != nil {
+				_ = json.NewEncoder(w).Encode(response)
+			}
+
+			return
+		}),
+	)
+
+	l, _ := net.Listen("tcp", testPort)
+	ts.Listener = l
+
+	return ts
+}
+
 func getTestClient() *Client {
-	client := NewClient(TestBaseUrl, TestApiSecret)
+	client := NewClient(testBaseUrl, testApiSecret)
 	client.SetLogWriter(ioutil.Discard)
 	return client
 }
 
 func TestHankoApiClient_NewHttpRequestWithHmac(t *testing.T) {
 	client := getTestClient()
-	client.SetHmac(TestHmacApiKeyId)
+	client.SetHmac(testHmacApiKeyId)
 	requestBody := &struct{ foo string }{"bar"}
 	request, err := client.NewHttpRequest(http.MethodPost, "/test", &requestBody)
 	if err != nil {
@@ -43,7 +84,7 @@ func TestHankoApiClient_NewHttpRequestWithoutHmac(t *testing.T) {
 		t.Fail()
 	}
 	authorizationHeader := request.Header.Get("Authorization")
-	if authorizationHeader != fmt.Sprintf("secret %s", TestApiSecret) {
+	if authorizationHeader != fmt.Sprintf("secret %s", testApiSecret) {
 		t.Errorf("wrong authorization header, got: %s", authorizationHeader)
 		t.Fail()
 	}
@@ -51,9 +92,9 @@ func TestHankoApiClient_NewHttpRequestWithoutHmac(t *testing.T) {
 
 func TestHankoApiClient_Do(t *testing.T) {
 	client := getTestClient()
-	httpRequest, err := http.NewRequest(http.MethodPost, TestBaseUrl, nil)
+	httpRequest, err := http.NewRequest(http.MethodPost, testBaseUrl, nil)
 
-	ts := RunTestApi(nil, nil, http.StatusOK)
+	ts := runTestApi(nil, nil, http.StatusOK)
 	ts.Start()
 	_, err = client.HttpClientDo(httpRequest)
 	if err != nil {
@@ -62,7 +103,7 @@ func TestHankoApiClient_Do(t *testing.T) {
 	}
 	ts.Close()
 
-	ts = RunTestApi(nil, nil, http.StatusCreated)
+	ts = runTestApi(nil, nil, http.StatusCreated)
 	ts.Start()
 	_, err = client.HttpClientDo(httpRequest)
 	if err != nil {
@@ -71,7 +112,7 @@ func TestHankoApiClient_Do(t *testing.T) {
 	}
 	ts.Close()
 
-	ts = RunTestApi(nil, nil, http.StatusBadRequest)
+	ts = runTestApi(nil, nil, http.StatusBadRequest)
 	ts.Start()
 	_, err = client.HttpClientDo(httpRequest)
 	if err == nil {
