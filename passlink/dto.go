ebe4b6ca093281b3517ffc42288d1fa3e296c1b5
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2021-09-21 14:53:28 +0200

    feat(passlink): adjust link and linkrequest dtos

--- passlink/dto.go
+++ passlink/dto.go
@@ -6,22 +6,21 @@ import (
 )
 
 type LinkRequest struct {
-	Email        string `json:"email"`
-	Transport    string `json:"transport"`
-	Template     string `json:"template"`
-	TemplateLang string `json:"lang"`
-	TTL          string `json:"ttl"`
-	RedirectTo   string `json:"redirect_to"`
+	UserID     string `json:"user_id"`
+	Transport  string `json:"transport"`
+	Phone      string `json:"phone_no"`
+	Email      string `json:"email"`
+	Template   string `json:"template"`
+	Locale     string `json:"locale"`
+	Timezone   string `json:"timezone"`
+	Salutation string `json:"salutation"`
+	TTL        string `json:"ttl"`
+	RedirectTo string `json:"redirect_to"`
 }
 
 type Link struct {
-	ID           uuid.UUID `json:"id"`
-	Email        string    `json:"email"`
-	Status       string    `json:"status"`
-	RedirectTo   string    `json:"redirect_to"`
-	ValidUntil   time.Time `json:"valid_until"`
-	Template     string    `json:"template"`
-	TemplateLang string    `json:"template_lang"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID         uuid.UUID `json:"id"`
+	UserID     string    `json:"user_id"`
+	Status     string    `json:"status"`
+	ValidUntil time.Time `json:"valid_until"`
 }
